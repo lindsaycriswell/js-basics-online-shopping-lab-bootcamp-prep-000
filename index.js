f42f51dcda1f8a81f7d9fe0fce1d712c07fff693
Lindsay Criswell <lindsaycriswell@gmail.com>
2017-10-09 21:55:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -50,6 +50,18 @@ function total() {
   }
   return total;
 }
+
+function removeFromCart(item) {
+  for(var i = 0; i < cart.length; i++){
+    if(cart[i].hasOwnProperty(item)){
+      cart.splice(i, 1);
+      return cart;
+    } else {
+      console.log('That item is not in your cart.');
+      return cart;
+    }
+  }
+}
 //
 // function removeFromCart(item) {
 //   for(var i = 0; i < cart.length; i++){
